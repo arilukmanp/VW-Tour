34490ceff084c4713f85c6eb05f7cbe5ba2c3ff7
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-11 17:10:35 +0700

    Add FAQ Data

---
+++ lib/models/faq.ts
@@ -0,0 +1,33 @@
+export interface FaqInterface {
+  question: string;
+  answer: string;
+}
+
+export const faqData: FaqInterface[] = [
+  {
+    question: "Berapa harga trip VW Borobudur?",
+    answer: "Harga mulai dari 350k untuk setiap VW",
+  },
+  {
+    question: "Dalam satu VW muat berapa orang?",
+    answer: "Maksimal untuk 4 orang penumpang dewasa dan 1 anak max 3 tahun",
+  },
+  {
+    question: "Apakah ada batas jumlah sewa unit?",
+    answer: "Tidak ada batas jumlah sewa unit VW Borobudur",
+  },
+  {
+    question: "Fasilitas apa yang di dapat?",
+    answer: "Sopir , BBM, Penjemputan di penginpan/resto sekitar Borobudur",
+  },
+  {
+    question: "Apakah ada jadwal keberangkatan?",
+    answer:
+      "Tidak ada, jadwal dapat diatur sesuai keinginan Anda dengan reservasi terlebih dahulu",
+  },
+  {
+    question: "Meeting point ada dimana kak?",
+    answer:
+      "Meeting point di Balkondes Borobudur, atau bisa juga dilakukan penjemputan di penginapan dan penjemputan di restoran sekitar Borobudur",
+  },
+];
