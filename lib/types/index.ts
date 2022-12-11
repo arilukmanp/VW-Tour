82f6f713e717b44da2215410991f686ba3adfff0
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-11 17:08:58 +0700

    Add Global Types

---
+++ lib/types/index.ts
@@ -0,0 +1,22 @@
+export enum TripCategory {
+  Short,
+  Medium,
+  Long,
+  OneDay,
+}
+
+export enum DestinationCategory {
+  Tour,
+  Education,
+  Resto,
+}
+
+export enum AdditionalItem {
+  Balloon,
+  Flare,
+  Happy,
+  Vacation,
+  Holiday,
+  Corporate,
+  Vip,
+}
