25c12d57e3ae15cd3a65e1f8e46a2acce58c3081
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-11 17:11:22 +0700

    Update FAQ Data

--- lib/models/faq.ts
+++ lib/models/faq.ts
@@ -30,4 +30,38 @@ export const faqData: FaqInterface[] = [
     answer:
       "Meeting point di Balkondes Borobudur, atau bisa juga dilakukan penjemputan di penginapan dan penjemputan di restoran sekitar Borobudur",
   },
+  {
+    question: "Apakah ada dokumentasinya?",
+    answer:
+      "Harga trip belum termasuk dokumentasi. Jika anda ingin dokumentasi, terdapat berbagai pilihan paket dokumentasi mulai dari 350k",
+  },
+  {
+    question: "Rutenya kemana aja kak?",
+    answer:
+      "Anda dapat memilih beberapa destinasi wisata dengan pilihan yang sudah tersedia dan akan melewati beberapa spot foto serta rute pedesaan atau persawahan",
+  },
+  {
+    question: "Bagaimana cara booking-nya kak?",
+    answer:
+      "Anda dapat memilih paket Trip yang ada terlebih dahulu, lalu pilih Destinasi yang tersedia. Ada juga paket Tambahan yang dapat anda pilih sesuai dengan keinginan",
+  },
+  {
+    question: "Apakah ada batas waktu untuk booking VW Borobudur?",
+    answer:
+      "Tidak ada batas waktu. Namun sebaiknya lakukan booking jauh hari sebelumnya, terutama untuk keberangkatan dihari libur",
+  },
+  {
+    question: "Apakah perlu DP? Jika ada berapa?",
+    answer: "Perlu, minimal DP 30%",
+  },
+  {
+    question: "Apakah harga Trip sudah include tiket destinasi wisata?",
+    answer:
+      "Belum, harga Trip belum termasuk tiket untuk destinasi wisata. Terdapat beberapa destinasi yang memerlukan biaya tambahan untuk tiket",
+  },
+  {
+    question: "Bagaimana jika seandainya hujan?",
+    answer:
+      "Mobil VW safari ini terdapat atap yang dapat dibuka dan ditutup sehingga tidak kehujanan",
+  },
 ];
