aea6f33ae42c440502a4c95a86b73680ba9b6c53
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-12 19:10:31 +0700

    Add Trip Data

---
+++ lib/models/trip.ts
@@ -0,0 +1,58 @@
+import { TripCategory } from "lib/types";
+
+export interface TripInterface {
+  category: TripCategory;
+  image: string;
+  title: string;
+  description: string;
+  duration: number;
+  destination: number;
+  price: number;
+}
+
+export const tripData: TripInterface[] = [
+  {
+    category: TripCategory.Short,
+    image:
+      "https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500",
+    title: "Short Trip",
+    description:
+      "Keliling perdesaan di sekitar Borobudur dan mengunjungi 2 pilihan destinasi wisata dengan durasi sekitar 2.5 jam.",
+    duration: 2.5,
+    destination: 2,
+    price: 350_000,
+  },
+  {
+    category: TripCategory.Medium,
+    image:
+      "https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500",
+    title: "Medium Trip",
+    description:
+      "Keliling perdesaan di sekitar Borobudur dan mengunjungi 3 pilihan destinasi wisata dengan durasi sekitar 3.5 jam.",
+    duration: 3.5,
+    destination: 3,
+    price: 450_000,
+  },
+  {
+    category: TripCategory.Long,
+    image:
+      "https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500",
+    title: "Long Trip",
+    description:
+      "Keliling perdesaan di sekitar Borobudur dan mengunjungi 4 pilihan destinasi wisata dengan durasi sekitar 5 jam.",
+    duration: 5,
+    destination: 4,
+    price: 700_000,
+  },
+  {
+    category: TripCategory.OneDay,
+    image:
+      "https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500",
+    title: "One Day",
+    description:
+      "Keliling perdesaan di sekitar Borobudur dan mengunjungi 7 pilihan destinasi wisata dengan durasi sekitar 9 jam.",
+    duration: 9,
+    destination: 7,
+    price: 900_000,
+  },
+];
