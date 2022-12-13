e36f3b02aa393ec1572d3c15746f1563b2ad729b
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-13 19:39:12 +0700

    Update Destinations Data

--- lib/models/destinations.ts
+++ lib/models/destinations.ts
@@ -41,4 +41,57 @@ export const destinationsData: DestinationInterface[] = [
     location: "Karangrejo, Kec. Borobudur",
     price: 20_000,
   },
+  {
+    category: DestinationCategory.Tour,
+    image: [
+      "junkyard/junkyard_1.webp",
+      "junkyard/junkyard_2.webp",
+      "junkyard/junkyard_3.webp",
+    ],
+    title: "Junkyard Auto Park",
+    description:
+      "Junkyard Autopark & Coffee sebuah destinasi wisata baru bertema otomotif dengan menyajikan mobil-mobil bekas yang cocok buat jadi spot foto kekinian. Di tempat ini ada puluhan mobil klasik yang bisa kamu jadikan latar foto kamu.",
+    location: "Wanurejo, Kec. Borobudur",
+    price: 20_000,
+  },
+  {
+    category: DestinationCategory.Tour,
+    image: [
+      "svarga-bumi/svarga_1.webp",
+      "svarga-bumi/svarga_2.webp",
+      "svarga-bumi/svarga_3.webp",
+      "svarga-bumi/svarga_4.webp",
+    ],
+    title: "Svarga Bumi",
+    description:
+      "Svargabumi merupakan tempat alternatif wisata yang berada di sekitar candi Borobudur. Svargabumi menyuguhkan pemandagan lahan persawahan petani yang dihiasi spot foto menarik bagi pengunjung. Selain itu pemandangan candi Borobudur yang tampak gagah di sisi timur menambah pesona dari tempat ini.",
+    location: "Jl. Borobudur-Ngadiharjo, Kec. Borobudur",
+    price: 30_000,
+  },
+  {
+    category: DestinationCategory.Resto,
+    image: [
+      "enam-langit/enamlangit_1.webp",
+      "enam-langit/enamlangit_2.webp",
+      "enam-langit/enamlangit_3.webp",
+      "enam-langit/enamlangit_4.webp",
+    ],
+    title: "Enam Langit by Plataran",
+    description:
+      "Enam Langit adalah restoran dan cafe cocok untuk tempat bersantai, berdiri setinggi tiga meter dan bertengger di tepi tebing. Anda dapat melihat matahari terbit dan terbenam di enam gunung paling terkenal: Merapi, Merbabu, Telomoyo, Andong, Sumbing dan Prau; dan Anda juga akan mendapatkan pemandangan indah candi Borobudur dari sini.",
+    location: "Candirejo, Kec. Borobudur",
+  },
+  {
+    category: DestinationCategory.Resto,
+    image: [
+      "dagi/dagi_1.webp",
+      "dagi/dagi_2.webp",
+      "dagi/dagi_3.webp",
+      "dagi/dagi_4.webp",
+    ],
+    title: "Dagi Hill by Manohara",
+    description:
+      "Dagi Hill by Manohara merupakan resto yang menawarkan view candi dengan latar belakang pegunungan Menoreh di sisi selatan dari atas bukit Dagi, menikmati sajian masakan bertema outdoor picnic set.",
+    location: "Taman Wisata Candi Borobudur",
+  },
 ];
