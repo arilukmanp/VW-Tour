23906362909638e7d0ac8692461d22fb394d868b
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-14 13:47:56 +0700

    Update Currency Formatter with additional K-Formatter

--- lib/utils/currencyFormatter.ts
+++ lib/utils/currencyFormatter.ts
@@ -1,3 +1,9 @@
 export function currencyFormatter(amount: number): string {
   return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+export function KFormatter(amount: number): string {
+  return Math.abs(amount) > 999
+    ? Math.sign(amount) * Number((Math.abs(amount) / 1000).toFixed(1)) + "k"
+    : (Math.sign(amount) * Math.abs(amount)).toString();
+}
