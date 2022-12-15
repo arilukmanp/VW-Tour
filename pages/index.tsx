7a985a2e7ea2c8c3d29de23d18581064c02e20ca
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-15 20:31:33 +0700

    Add Viewport to Prevent Zooming When Input is Active

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,10 @@ export default function Home() {
           name="description"
           content="Jangan lewatkan pengalaman menjelajahi keindahan dan budaya di sekitar Borobudur dengan cara yang menyenangkan bersama VW Wisata Borobudur!"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
