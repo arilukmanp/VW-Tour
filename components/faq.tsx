d047f503a67190f00c8f1e7fa3dc9a72a6cc15c4
Ari Lukman P <ari.lukman.p@gmail.com>
2022-12-17 21:32:51 +0700

    Add Faq Section

---
+++ components/faq.tsx
@@ -0,0 +1,22 @@
+export default function Faq() {
+  return (
+    <section id="faq">
+      <div className="bg-white">
+        <div className="container px-5 md:px-8 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-4xl text-3xl font-semibold mb-4 text-gray-900">
+              FAQ
+            </h1>
+            <p className="lg:w-2/3 xl:w-1/2 mx-auto leading-relaxed text-base text-gray-500">
+              Pertanyaan Yang Sering Ditanyakan
+            </p>
+          </div>
+
+          <div className="mt-6 space-y-1 xl:mt-12">
+            {/* content */}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
