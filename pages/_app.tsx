77632effb0d057e13364d8210297ada0f822457d
Ari Lukman P <ari.lukman.p@gmail.com>
2023-01-07 04:50:07 +0700

    Implement Cart Provider into Root App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import CartProvider from "lib/context/CartProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <CartProvider>
       <Component {...pageProps} />;
       <Analytics />
-    </>
+    </CartProvider>
   );
 }
