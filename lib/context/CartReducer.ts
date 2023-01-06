afae1e4e68f8dd2097db0fad4d5fb7fb10a023ff
Ari Lukman P <ari.lukman.p@gmail.com>
2023-01-07 04:48:58 +0700

    Add Cart Reducer

---
+++ lib/context/CartReducer.ts
@@ -0,0 +1,63 @@
+import { useCallback, useReducer } from "react";
+import { TripInterface } from "lib/models/trip";
+import { AdditionalInterface } from "lib/models/additionals";
+import { DestinationInterface } from "lib/models/destinations";
+
+interface AdditionalCartInterface {
+  data: AdditionalInterface;
+  amount: number;
+}
+
+type CartType = {
+  people: number | null;
+  trip: TripInterface;
+  destinations: DestinationInterface[];
+  additionals: AdditionalCartInterface[];
+};
+
+type ActionType =
+  | { type: "SET_PEOPLE"; people: number }
+  | { type: "SET_TRIP"; trip: TripInterface };
+
+type CartReducerType = {
+  people: number | null;
+  setPeople: (people: number) => void;
+  trip: TripInterface;
+  setTrip: (trip: TripInterface) => void;
+  destinations: DestinationInterface[];
+  additionals: AdditionalCartInterface[];
+};
+
+const initialCartData: CartType = {
+  people: null,
+  trip: {} as TripInterface,
+  destinations: [] as DestinationInterface[],
+  additionals: [] as AdditionalCartInterface[],
+};
+
+export default function useCartReducer(): CartReducerType {
+  const reducer = (state: CartType, action: ActionType) => {
+    switch (action.type) {
+      case "SET_PEOPLE":
+        return { ...state, people: action.people };
+
+      case "SET_TRIP":
+        return { ...state, trip: action.trip };
+    }
+  };
+
+  const [{ people, trip, destinations, additionals }, dispatch] = useReducer(
+    reducer,
+    initialCartData
+  );
+
+  const setPeople = useCallback((people: number) => {
+    dispatch({ type: "SET_PEOPLE", people: people });
+  }, []);
+
+  const setTrip = useCallback((trip: TripInterface) => {
+    dispatch({ type: "SET_TRIP", trip: trip });
+  }, []);
+
+  return { people, setPeople, trip, setTrip, destinations, additionals };
+}
