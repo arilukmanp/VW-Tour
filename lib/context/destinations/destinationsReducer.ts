cd93723157ecd2c1afe33ff97394d933b5c16e0f
Ari Lukman P <ari.lukman.p@gmail.com>
2023-01-08 10:56:24 +0700

    Add Destinations Store and Reducer

---
+++ lib/context/destinations/destinationsReducer.ts
@@ -0,0 +1,56 @@
+import { DestinationInterface } from "lib/models/destinations";
+import { useCallback, useReducer } from "react";
+
+type StateType = {
+  isShowedDetail: boolean;
+  destination: DestinationInterface;
+};
+
+type ActionType =
+  | { type: "SHOW_DETAIL"; destination: DestinationInterface }
+  | { type: "DISMISS_DETAIL" };
+
+type ReducerType = {
+  isShowedDetail: boolean;
+  destination: DestinationInterface;
+  showDetail: (destination: DestinationInterface) => void;
+  dismissDetail: () => void;
+};
+
+const initialData: StateType = {
+  isShowedDetail: false,
+  destination: {} as DestinationInterface,
+};
+
+export default function useDestinationsReducer(): ReducerType {
+  const reducer = (state: StateType, action: ActionType) => {
+    switch (action.type) {
+      case "SHOW_DETAIL":
+        return {
+          destination: action.destination,
+          isShowedDetail: true,
+        };
+
+      case "DISMISS_DETAIL":
+        return {
+          ...state,
+          isShowedDetail: false,
+        };
+    }
+  };
+
+  const [{ isShowedDetail, destination }, dispatch] = useReducer(
+    reducer,
+    initialData
+  );
+
+  const showDetail = useCallback((destination: DestinationInterface) => {
+    dispatch({ type: "SHOW_DETAIL", destination: destination });
+  }, []);
+
+  const dismissDetail = useCallback(() => {
+    dispatch({ type: "DISMISS_DETAIL" });
+  }, []);
+
+  return { isShowedDetail, destination, showDetail, dismissDetail };
+}
---
+++ lib/context/destinations/destinationsStore.ts
@@ -0,0 +1,8 @@
+import { createContext, useContext } from "react";
+import useDestinationsReducer from "./destinationsReducer";
+
+type ContextType = ReturnType<typeof useDestinationsReducer>;
+const initialContext = {} as unknown as ContextType;
+
+export const DestinationsContext = createContext<ContextType>(initialContext);
+export const useDestinationContext = () => useContext(DestinationsContext);
