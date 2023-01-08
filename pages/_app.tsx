f9451582a7ce7c021f77ad8c67b545e64a067d44
Ari Lukman P <ari.lukman.p@gmail.com>
2023-01-08 11:10:05 +0700

    Implement Destinations Provider into root app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import CartProvider from "lib/context/CartProvider";
+import DestinationDetail from "components/destinationDetail";
+import DestinationsProvider from "lib/context/destinations/destinationsProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
-      <Component {...pageProps} />;
+      <DestinationsProvider>
+        <Component {...pageProps} />;
+        <DestinationDetail />
+      </DestinationsProvider>
       <Analytics />
     </CartProvider>
   );
