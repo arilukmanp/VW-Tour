1da95ff373be91b2311ac0324d9345ed0a06543f
Ari Lukman P <ari.lukman.p@gmail.com>
2023-01-08 10:57:03 +0700

    Add Destinations Context Provider

---
+++ lib/context/destinations/destinationsProvider.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import useDestinationsReducer from "./destinationsReducer";
+import { DestinationsContext } from "./destinationsStore";
+
+interface PropsInterface {
+  children: ReactNode;
+}
+
+export default function DestinationsProvider({ children }: PropsInterface) {
+  const value = useDestinationsReducer();
+
+  return (
+    <DestinationsContext.Provider value={value}>
+      {children}
+    </DestinationsContext.Provider>
+  );
+}
