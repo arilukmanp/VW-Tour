e1c07d2f3a9bf5ca7fc5d0140eaf8d1259759ecc
Ari Lukman P <ari.lukman.p@gmail.com>
2023-01-09 15:12:26 +0700

    Update destination detail's content

--- components/destinationDetail.tsx
+++ components/destinationDetail.tsx
@@ -5,13 +5,12 @@ import Image from "next/image";
 import { Dialog, Transition } from "@headlessui/react";
 import { BottomSheet } from "react-spring-bottom-sheet";
 import useScreenMobile from "lib/hooks/useScreenMobile";
+import { useCartContext } from "lib/context/CartStore";
+import { useDestinationContext } from "lib/context/destinations/destinationsStore";
 import { DestinationInterface } from "lib/models/destinations";
-
-interface DestinationDetailInterface {
-  data: DestinationInterface;
-  isShow: boolean;
-  setIsShow: Dispatch<boolean>;
-}
+import { DestinationCategory } from "lib/types";
+import { KFormatter } from "lib/utils/currencyFormatter";
+import Alert from "./alert";
 
 interface ContentInterface {
   data: DestinationInterface;
@@ -19,13 +18,11 @@ interface ContentInterface {
   setSelectedIdxImage: Dispatch<number>;
 }
 
-export default function DestinationDetail({
-  data,
-  isShow,
-  setIsShow,
-}: DestinationDetailInterface) {
-  const [selectedIdxImage, setSelectedIdxImage] = useState(0);
+export default function DestinationDetail() {
   const isMobile = useScreenMobile();
+  const { destination, isShowedDetail, dismissDetail } =
+    useDestinationContext();
+  const [selectedIdxImage, setSelectedIdxImage] = useState(0);
 
   useEffect(() => {
     let delay = setTimeout(() => {
@@ -35,13 +32,13 @@ export default function DestinationDetail({
     return () => {
       delay;
     };
-  }, [isShow]);
+  }, [isShowedDetail]);
 
   if (isMobile) {
     return (
-      <BottomSheet open={isShow} onDismiss={() => setIsShow(false)}>
+      <BottomSheet open={isShowedDetail} onDismiss={() => dismissDetail()}>
         <Content
-          data={data}
+          data={destination}
           selectedIdxImage={selectedIdxImage}
           setSelectedIdxImage={setSelectedIdxImage}
         />
@@ -50,8 +47,8 @@ export default function DestinationDetail({
   }
 
   return (
-    <Transition show={isShow} as={Fragment}>
-      <Dialog onClose={() => setIsShow(false)} className="relative z-50">
+    <Transition show={isShowedDetail} as={Fragment}>
+      <Dialog onClose={() => dismissDetail()} className="relative z-50">
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -76,7 +73,7 @@ export default function DestinationDetail({
           <div className="fixed inset-0 flex items-center justify-center p-4">
             <Dialog.Panel className="w-full max-w-5xl rounded-xl bg-white">
               <Content
-                data={data}
+                data={destination}
                 selectedIdxImage={selectedIdxImage}
                 setSelectedIdxImage={setSelectedIdxImage}
               />
@@ -90,62 +87,147 @@ export default function DestinationDetail({
 
 function Content(props: ContentInterface) {
   const { data, selectedIdxImage, setSelectedIdxImage } = props;
+  const { dismissDetail } = useDestinationContext();
+  const { trip, destinations, addDestination, removeDestination } =
+    useCartContext();
+  const [isAlertShowed, setIsAlertShowed] = useState(false);
+  const [alertMessage, setAlertMessage] = useState(<></>);
+
+  const isSelected = destinations.find(
+    (destination) => destination.title == data.title
+  );
+
+  function onSelect() {
+    if (!trip.category) {
+      setAlertMessage(
+        <span>
+          Silakan pilih <u>Paket Trip</u> terlebih dahulu
+        </span>
+      );
+      setIsAlertShowed(true);
+      return;
+    }
+
+    if (isSelected) {
+      removeDestination(data);
+    } else {
+      if (destinations.length >= trip.totalDestination) {
+        setAlertMessage(
+          <span>
+            Destinasi melebihi kuota. Anda bisa menghapus destinasi lain dulu
+          </span>
+        );
+        setIsAlertShowed(true);
+        return;
+      }
+
+      addDestination(data);
+    }
+
+    dismissDetail();
+  }
+
+  const AdditionalInformation = () => {
+    let isEducation = data.category == DestinationCategory.Education;
+    let isRestaurant = data.category == DestinationCategory.Resto;
+    let isTour = data.category == DestinationCategory.Tour;
+    let hasAdditionalPrice = Boolean(data?.price);
+
+    if (isEducation && hasAdditionalPrice) {
+      return (
+        <p>
+          Terdapat tambahan biaya sekitar Rp{" "}
+          <span className="font-semibold">{KFormatter(data.price!)}</span>/orang
+          apabila ingin praktik
+        </p>
+      );
+    } else if (isTour && hasAdditionalPrice) {
+      return (
+        <p>
+          Terdapat tambahan biaya untuk tiket masuk sekitar Rp{" "}
+          <span className="font-semibold">{KFormatter(data.price!)}</span>/orang
+        </p>
+      );
+    } else if (isRestaurant) {
+      return <p>Biaya tour tidak termasuk makan dan minum di tempat ini</p>;
+    } else {
+      return <p>Tidak ada biaya tambahan untuk destinasi ini</p>;
+    }
+  };
 
   return (
-    <div className="p-4 mx-auto text-gray-600 body-font overflow-hidden">
-      <div className="mx-auto flex flex-wrap">
-        <div className="relative lg:w-3/5 w-full lg:h-auto aspect-[4/3] bg-slate-100 rounded">
-          <Image
-            src={`/images/destinations/${data.images[selectedIdxImage]}`}
-            alt={`Foto ${data.title}`}
-            layout={"fill"}
-            quality={50}
-            className={"object-cover object-center rounded"}
-          />
-        </div>
+    <>
+      <div className="p-4 mx-auto text-gray-600 body-font overflow-hidden">
+        <div className="mx-auto flex flex-wrap">
+          <div className="relative lg:w-3/5 w-full lg:h-auto aspect-[4/3] bg-slate-100 rounded">
+            <Image
+              src={`/images/destinations/${data.images[selectedIdxImage]}`}
+              alt={`Foto ${data.title}`}
+              layout={"fill"}
+              quality={50}
+              className={"object-cover object-center rounded"}
+            />
+          </div>
 
-        <div className="lg:w-2/5 w-full lg:pl-6 lg:py-2 mt-6 lg:mt-0">
-          <div className="flex flex-col">
-            <h1 className="text-slate-900 text-3xl font-semibold mb-2">
-              {data.title}
-            </h1>
+          <div className="lg:w-2/5 w-full lg:pl-6 lg:py-2 mt-6 lg:mt-0">
+            <div className="flex flex-col">
+              <h1 className="text-slate-900 text-3xl font-semibold mb-2">
+                {data.title}
+              </h1>
 
-            <div className="flex mb-4">
-              <div className="px-2 py-0.5 sm:py-1 text-xs text-cyan-500 bg-slate-100 bg-opacity-90 rounded-sm sm:rounded-md">
-                {data.category.valueOf()}
+              <div className="flex mb-4">
+                <div className="px-2 py-0.5 sm:py-1 text-xs text-cyan-500 bg-slate-100 bg-opacity-90 rounded-sm sm:rounded-md">
+                  {data.category.valueOf()}
+                </div>
               </div>
-            </div>
 
-            <p className="leading-relaxed text-sm text-gray-500 mt-4">
-              {data.description}
-            </p>
+              <p className="leading-relaxed text-sm text-gray-500 mt-4">
+                {data.description}
+              </p>
 
-            <div className="flex mt-3 items-center pb-5 order-first lg:order-none ">
-              <div className="relative flex py-3 overflow-x-auto">
-                {data.images.map((image, index) => (
-                  <img
-                    key={index.toString()}
-                    onClick={() => setSelectedIdxImage(index)}
-                    src={`/images/destinations/${image}`}
-                    alt={`Foto ${data.title}`}
-                    className={`w-16 mx-1 aspect-square cursor-pointer rounded-lg bg-slate-100 object-cover object-center ${
-                      selectedIdxImage == index
-                        ? "border-2 border-orangeSoft"
-                        : "p-[2px]"
-                    }`}
-                  />
-                ))}
+              <div className="flex mt-3 items-center pb-5 order-first lg:order-none ">
+                <div className="relative flex py-3 overflow-x-auto">
+                  {data.images.map((image, index) => (
+                    <img
+                      key={index.toString()}
+                      onClick={() => setSelectedIdxImage(index)}
+                      src={`/images/destinations/${image}`}
+                      alt={`Foto ${data.title}`}
+                      className={`w-16 mx-1 aspect-square cursor-pointer rounded-lg bg-slate-100 object-cover object-center ${
+                        selectedIdxImage == index
+                          ? "border-2 border-orangeSoft"
+                          : "p-[2px]"
+                      }`}
+                    />
+                  ))}
+                </div>
               </div>
-            </div>
 
-            <div className="flex border-t-2 border-gray-100 pt-5 mt-5">
-              <button className="flex mx-auto text-white bg-orangeSoft border-0 py-2 px-4 focus:outline-none hover:bg-orange-500 rounded-md">
-                Pilih Destinasi Ini
-              </button>
+              <div className="flex flex-row items-start text-xs text-orangeSoft mt-4">
+                <div className="text-sm leading-tight mr-0.5">*</div>
+                <AdditionalInformation />
+              </div>
+
+              <div className="flex border-t-2 border-gray-100 pt-5 mt-5">
+                <button
+                  onClick={() => onSelect()}
+                  className="flex mx-auto text-white bg-orangeSoft border-0 py-2 px-4 focus:outline-none hover:bg-orange-500 rounded-md"
+                >
+                  {isSelected
+                    ? "Batalkan Destinasi Ini"
+                    : "Pilih Destinasi Ini"}
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Alert
+        message={alertMessage}
+        isShowed={isAlertShowed}
+        setIsShowed={setIsAlertShowed}
+      />
+    </>
   );
 }
