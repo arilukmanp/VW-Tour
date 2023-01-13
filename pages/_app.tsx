26f368f86c9333227f7eef44eb539f6cdb7a2fb2
Ari Lukman P <ari.lukman.p@gmail.com>
2023-01-13 15:27:08 +0700

    Bug fix after refactor destination detail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import CartProvider from "lib/context/cart/CartProvider";
-import DestinationDetail from "components/destinationDetail";
 import DestinationsProvider from "lib/context/destinations/destinationsProvider";
+import DestinationDetail from "components/parts/common/destinationDetail";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
