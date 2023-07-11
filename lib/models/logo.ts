4ccf1279a53a354fb8e2062ff9a2b17b52267d89
Ari Lukman P <ari.lukman.p@gmail.com>
2023-07-11 15:37:54 +0700

    Add mock logo clouds data

---
+++ lib/models/logo.ts
@@ -0,0 +1,24 @@
+export interface LogoCloudsInterface {
+  title: string;
+  image: string;
+}
+
+export const logoCloudsData: LogoCloudsInterface[] = [
+  {
+    title: "Transistor",
+    image:
+      "https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg",
+  },
+  {
+    title: "Reform",
+    image: "https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg",
+  },
+  {
+    title: "Tuple",
+    image: "https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg",
+  },
+  {
+    title: "SavvyCal",
+    image: "https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg",
+  },
+];
