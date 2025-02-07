bd87e851278c1baf3610436145de14c978b322c4
Ari Lukman P <ari.lukman.p@gmail.com>
2025-02-08 00:18:56 +0700

    Config for cloudflare worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
 import CartProvider from "lib/context/cart/CartProvider";
 import DestinationsProvider from "lib/context/destinations/destinationsProvider";
 import DestinationDetail from "components/parts/common/destinationDetail";
@@ -12,7 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
         <DestinationDetail />
       </DestinationsProvider>
-      <Analytics />
     </CartProvider>
   );
 }
